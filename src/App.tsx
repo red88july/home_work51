a454c7af6c696fdaedf63eb283052caf7ec66fb6
Sergeev Mikhail <sergeev-michael@mail.ru>
2023-10-18 21:35:03 +0600

    Correct keys on Props in  Number.tsx file then correct line from 18-22 on App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,24 @@ import "./App.css";
 import Number from "./Number/Number.tsx";
 import {useState} from "react";
 
-
 function App () {
-
     const [number , stateNumber] = useState([
-        {numeric: 5},
-        {numeric: 11},
-        {numeric: 16},
-        {numeric: 23},
-        {numeric: 32},
+        {numfirst: 5, numsecond: 11, numthird: 16, numfourth: 23, numfifth: 32}
     ]);
 
     const ChangeNumber = () => {
         stateNumber([
-            {numeric: 1, },
-            {numeric: 42},
-            {numeric: 160},
-            {numeric: 67},
-            {numeric: 35},
+            {numfirst: 1, numsecond: 42, numthird: 156, numfourth: 45, numfifth: 2}
         ]);
     };
 
     return (
         <div className='App'>
-            <Number numeric={number[0].numeric}></Number>
-            <Number numeric={number[1].numeric}></Number>
-            <Number numeric={number[2].numeric}></Number>
-            <Number numeric={number[3].numeric}></Number>
-            <Number numeric={number[4].numeric}></Number>
+            <Number numfirst={number[0].numfirst}
+                    numsecond={number[0].numsecond}
+                    numthird={number[0].numthird}
+                    numfourth={number[0].numfourth}
+                    numfifth={number[0].numfifth}></Number>
             <button onClick={ChangeNumber}>Change number</button>
         </div>
     )
